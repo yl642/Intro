f5b45aa2464ec8a8b78daef7fc126357f51fb029
yl642 <yl642@duke.edu>
2020-02-05 19:35:28 -0500

    bad commit

--- file1.py
+++ file1.py
@@ -1,1 +1,21 @@
-print("Got ya!")
+# file1.py
+def sub(a, b):
+    if isinstance(a, (int, float, complex)) and not(isinstance(a, bool)):
+        if isinstance(b, (int, float, complex)) and not(isinstance(b, bool)):
+            return a - b
+    return False
+
+
+def min_max(x):
+    a = []
+    for i in x:
+        b = isinstance(i, (int, float, complex)) and not(isinstance(i, bool))
+        a.append(b)
+    if all(a):
+        x.sort()
+        return x[0], x[-1]
+    return False
+
+
+if __name__ == '__main__':
+    pass
---
+++ test_file1.py
@@ -0,0 +1,26 @@
+# test_file1.py
+import pytest
+
+
+@pytest.mark.parametrize('a, b, output, expected,', [
+    (0.5, 0.2, 0.3, True),
+    (2, 3, 5, False),
+    ('a', 5, False, True),
+    (2, True, False, True),
+])
+def test_sub(a, b, output, expected):
+    from file1 import sub
+    answer = sub(a, b) == output
+    assert answer == expected
+
+
+@pytest.mark.parametrize('x, output, expected,', [
+    ([6, 1, 5, 2, 10, 3, 4], (1, 10), True),  # output should be carried out as tuples
+    ([6, 1, 5, 2, True, 10, 3, 4], False, True),
+    ([6, 1, 5, 2, 'a', 10, 3, 4], False, True),
+    ([61, 1, 15, 2, 100, 10, 3, 4], (100, 1), False),
+])
+def test_min_max(x, output, expected):
+    from file1 import min_max
+    answer = min_max(x) == output
+    assert answer == expected
